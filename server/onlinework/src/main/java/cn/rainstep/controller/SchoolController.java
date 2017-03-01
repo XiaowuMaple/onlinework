ddf1cd4d770997df11b8eafb95ed84f9515e8a76
Zero <303228522@qq.com>
2017-03-01 09:42:19 +0800

    app index

--- server/onlinework/src/main/java/cn/rainstep/controller/SchoolController.java
+++ server/onlinework/src/main/java/cn/rainstep/controller/SchoolController.java
@@ -6,14 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
-import java.util.List;
+
 import java.util.Map;
 
 
